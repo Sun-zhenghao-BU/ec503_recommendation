032cf0ae39b250d72c5666c8c0a081515c9de8ca
ShuYangConlany <113554935+ShuYangConlany@users.noreply.github.com>
2022-11-20 17:29:11 -0500

    Update MABenv.py

--- Q-learning_MAB/MABenv.py
+++ Q-learning_MAB/MABenv.py
@@ -4,7 +4,10 @@ from numpy import random
 class MAB:
     def __init__(self,numOfMAB,learning_rate=0.01, reward_decay=0.9):
         self.Q = np.zeros(numOfMAB)
-        self.win = np.arange(numOfMAB)/numOfMAB
+        #self.win = np.arange(numOfMAB)/numOfMAB
+        #self.win = np.array([0.1,0.2,0.3,0.2,0.1,0.2,0.4,0.3,0.4,0.8])
+        self.win = np.array([0.1,0.2,0.3,0.2,0.1,0.2,0.4,0.3,0.7,0.8])
+
         self.pull = np.zeros(numOfMAB)
         self.alpha = 0.
         self.lr = learning_rate
@@ -15,11 +18,11 @@ class MAB:
         result = random.rand()
         self.pull[action] = self.pull[action]+1
         if result<self.win[action]:
-            r = 0.01
+            r = 0.1
         else:
-            r = -0.01
+            r = -0.1
         self.learn(a=action,s=1,r=r,s_=1)
 
 
-    def learn(self,a,s=1,r=0,s_=1):
+    def learn(self,a,s=1,r=0.0,s_=1):
         self.Q[a] += self.lr*(r + self.rd*max(self.Q) - self.Q[a])
